9382e8f988d6e32fb85631c9a229355d922a0b0d
Thierry <thierry.ouellet@icloud.com>
2023-03-10 15:37:08 -0500

    Fixed taxAmount bug

--- build.go
+++ build.go
@@ -413,8 +413,17 @@ func (doc *Document) appendTotal() {
 	var Taxes []Tax
 	for _, item := range doc.Items {
 		for _, tax := range item.Taxes {
-
-			tax._total = item.TotalWithoutTaxAndWithoutDiscount().Mul(tax._percent)
+			taxType, taxAmount, taxAmountForEach, _ := tax.getTax()
+			if taxType == TaxTypeAmount {
+				if taxAmountForEach {
+					tax._total = taxAmount.Mul(item._quantity)
+				} else {
+					tax._total = tax._total.Add(taxAmount)
+				}
+			} else {
+				tax._total = item.TotalWithoutTaxAndWithoutDiscount().Mul(tax._percent)
+				fmt.Println(item.TotalWithoutTaxAndWithoutDiscount())
+			}
 			Taxes = append(Taxes, tax)
 		}
 
